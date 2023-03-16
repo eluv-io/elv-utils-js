720e1bac9b3145fd2ac16ae774832299b1d11abe
eponymous301 <michael.parker@eluv.io>
2023-03-16 12:39:17 -0700

    Add VersionHashInfo.js script Update elv-client-js version

---
+++ utilities/VersionHashInfo.js
@@ -0,0 +1,38 @@
+// Retrieve part list from object
+const Utility = require('./lib/Utility')
+
+const Version = require('./lib/concerns/libs/Version')
+const {NewOpt} = require('./lib/options')
+
+class VersionHashInfo extends Utility {
+  static blueprint() {
+    return {
+      concerns: [Version],
+      options: [
+        NewOpt('versionHash', {
+          demand: true,
+          descTemplate: 'Version hash to decode',
+          type: 'string'
+        })
+      ]
+    }
+  }
+
+  async body() {
+    const {versionHash} = this.args
+    const result = this.concerns.Version.decode({versionHash})
+    this.logger.data('versionHashInfo', result)
+    // eslint-disable-next-line no-console
+    console.log(JSON.stringify(result, null, 2))
+  }
+
+  header() {
+    return `Print info for version hash ${this.args.versionHash}`
+  }
+}
+
+if(require.main === module) {
+  Utility.cmdLineInvoke(VersionHashInfo)
+} else {
+  module.exports = VersionHashInfo
+}
