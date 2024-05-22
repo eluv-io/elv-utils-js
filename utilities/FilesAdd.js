eluv-io/elv-utils-js

commit 61bfb5b1f73e6efa87b6cfdfcde664697727e1a1

* Fix smoke tests and package.json test script  * Move some arg concerns to subfolder: concerns/args  * Add ArgDestDir.js  * New concern ExistLibOrObjOrDft.js to support utilities that write/edit  * Add methods to Offering concern  * Delete unused files FinalizeAndWait.js, ObjectEdit.js  * Add PathStringModel.js  * Add helper dirListRecursive()  * Add --destDir option to FilesAdd.js, allow uploading to a draft  * Add --commitMsg to MetaDelete.js, allow utility to operate on draft or library  * Add --commitMsg to MetaSet.js, allow utility to operate on library

utilities/FilesAdd.js
@@ -1,11 +1,14 @@
 // Create new production master from specified file(s)
 const path = require('path')
 
+const {fabricItemDesc} = require('./lib/helpers')
+
 const {ModOpt, NewOpt} = require('./lib/options')
 const Utility = require('./lib/Utility')
 
+const ArgDestDir = require('./lib/concerns/args/ArgDestDir')
 const ArgNoWait = require('./lib/concerns/ArgNoWait')
-const ExistObj = require('./lib/concerns/kits/ExistObj')
+const ExistObjOrDft = require('./lib/concerns/kits/ExistObjOrDft')
 const CloudFile = require('./lib/concerns/CloudFile')
 const Edit = require('./lib/concerns/Edit')
 const LocalFile = require('./lib/concerns/LocalFile')
@@ -14,8 +17,9 @@ const Logger = require('./lib/concerns/Logger')
 class FilesAdd extends Utility {
   static blueprint() {
     return {
-      concerns: [Logger, ExistObj, Edit, ArgNoWait, LocalFile, CloudFile],
+      concerns: [Logger, ExistObjOrDft, Edit, ArgDestDir, ArgNoWait, LocalFile, CloudFile],
       options: [
+        ModOpt('destDir', {descTemplate: 'Destination directory within object (must start with \'/\'). Will be created if it does not exist.',}),
         ModOpt('files', {X: 'to add'}),
         NewOpt('storeClear', {
           descTemplate: 'If specified, files will use unencrypted storage',
@@ -27,7 +31,7 @@ class FilesAdd extends Utility {
 
   async body() {
     const logger = this.logger
-    const {storeClear, noWait} = this.args
+    const {storeClear, noWait, destDir} = this.args
 
     let access
     if(this.args.s3Reference || this.args.s3Copy) access = this.concerns.CloudFile.credentialSet()
@@ -37,9 +41,15 @@ class FilesAdd extends Utility {
       ? this.concerns.CloudFile.fileInfo()
       : this.concerns.LocalFile.fileInfo(fileHandles)
 
-    const {libraryId, objectId} = await this.concerns.ExistObj.argsProc()
+    if (destDir) {
+      for (const f of fileInfo) {
+        f.path = path.join(destDir, f.path)
+      }
+    }
 
-    const {writeToken} = await this.concerns.Edit.getWriteToken({
+    const {libraryId, objectId, writeToken} = await this.concerns.ExistObjOrDft.argsProc()
+
+    const suppliedOrNewWriteToken = writeToken || await this.concerns.Edit.getWriteToken({
       libraryId,
       objectId
     })
@@ -48,7 +58,7 @@ class FilesAdd extends Utility {
       await this.concerns.CloudFile.add({
         libraryId,
         objectId,
-        writeToken,
+        writeToken: suppliedOrNewWriteToken,
         access,
         fileInfo,
         encrypt: !storeClear
@@ -57,7 +67,7 @@ class FilesAdd extends Utility {
       await this.concerns.LocalFile.add({
         libraryId,
         objectId,
-        writeToken,
+        writeToken: suppliedOrNewWriteToken,
         fileInfo,
         encrypt: !storeClear
       })
@@ -67,26 +77,27 @@ class FilesAdd extends Utility {
 
     const fileBasenamesList = this.args.files.map(x => path.basename(x))
 
-    const hash = await this.concerns.Edit.finalize({
-      commitMessage: `Add files ${fileBasenamesList.join(', ')}`,
-      libraryId,
-      noWait,
-      objectId,
-      writeToken
-    })
-
     logger.logList(
       '',
-      'File(s) added.',
-      `New version hash: ${hash}`,
-      ''
+      'File(s) added.'
     )
 
-    logger.data('version_hash', hash)
+    if (!writeToken) {
+      const hash = await this.concerns.Edit.finalize({
+        commitMessage: `Add files ${fileBasenamesList.join(', ')}${destDir ? ` to folder ${destDir}` : ''}`,
+        libraryId,
+        noWait,
+        objectId,
+        writeToken: suppliedOrNewWriteToken
+      })
+      logger.log(`New version hash: ${hash}`)
+      logger.data('version_hash', hash)
+    }
+    logger.log('')
   }
 
   header() {
-    return `Add file(s) to object ${this.args.objectId}`
+    return `Add file(s) to ${this.args.destDir ? `directory '${this.args.destDir}' in `: ''}${fabricItemDesc(this.args)}`
   }
 }
 

test/test-helpers.js
@@ -1,11 +1,20 @@
-// helper to reduce boilerplate in tests
+// helpers to reduce boilerplate in tests
+
+const fs = require('fs')
 const path = require('path')
 
 const chai = require('chai')
 const sinon = require('sinon')
+
 const mergeDeepRight = require('@eluvio/elv-js-helpers/Functional/mergeDeepRight')
 
-const Utility = require('../lib/Utility')
+
+// Utilities that do not use framework
+const NONSTANDARD_UTILITIES = ['TextConvertForCmd.js', 'TextDecodeBase58.js']
+
+const {dirListRecursive} = require('../utilities/lib/helpers')
+
+const Utility = require('../utilities/lib/Utility')
 
 chai.should()
 const expect = chai.expect
@@ -19,14 +28,20 @@ const testEnv = {
 const argList2Params = (...argList) => {
   argList = argList || []
   argList = argList.concat('--silent')
-  argList = argList.concat('-v')
   return {
     argList,
     ...testEnv
   }
 }
 
-const concern2utility = (concernObject, argList) => {
+const concernList = () => dirListRecursive(
+  path.join(__dirname, '../utilities/lib/concerns')
+).filter(
+  f => path.extname(f) === '.js'
+)
+
+
+const concern2utility = concernObject => {
 
   class TestUtility extends Utility {
     static blueprint() {
@@ -44,7 +59,7 @@ const concern2utility = (concernObject, argList) => {
     }
   }
 
-  return new TestUtility(argList2Params(...argList))
+  return TestUtility
 }
 
 const params = testParams => mergeDeepRight(
@@ -58,13 +73,18 @@ const removeElvEnvVars = () => {
   delete process.env.PRIVATE_KEY
 }
 
-const requireConcern = subDirAndFilename => require(path.join(__dirname, '../lib/concerns', subDirAndFilename))
+const requireConcern = subDirAndFilename => path.isAbsolute(subDirAndFilename)
+  ? require(subDirAndFilename)
+  : require(path.join(__dirname, '../utilities/lib/concerns', subDirAndFilename))
 
-const requireUtility = subDirAndFilename => require(path.join(__dirname, '../', subDirAndFilename))
+const requireUtility = subDirAndFilename => require(path.join(__dirname, '../utilities', subDirAndFilename))
+
+const utilityFileList = () => fs.readdirSync(path.join(__dirname, '../utilities')).filter(f => path.extname(f) === '.js' && !NONSTANDARD_UTILITIES.includes(f))
 
 module.exports = {
   argList2Params,
   chai,
+  concernList,
   concern2utility,
   expect,
   params,
@@ -72,5 +92,6 @@ module.exports = {
   requireConcern,
   requireUtility,
   sinon,
-  testEnv
+  testEnv,
+  utilityFileList
 }

test/unit/smoke.test.js
@@ -1,0 +1,27 @@
+const TH = require('../test-helpers')
+
+describe('utilities', () => {
+  for (const u of TH.utilityFileList()) {
+    it(`${u} should return argMap successfully`, () => {
+      const klass = TH.requireUtility(u)
+      klass.argMap()
+    })
+  }
+})
+
+describe('concerns', () => {
+  for (const u of TH.concernList()) {
+    it(`${u} should load successfully`, () => {
+      TH.requireConcern(u)
+    })
+  }
+})
+
+describe('concerns as utilities', () => {
+  for (const u of TH.concernList()) {
+    it(`${u} should successfully load as a utility`, () => {
+      const klass = TH.concern2utility(TH.requireConcern(u), [])
+      klass.argMap()
+    })
+  }
+})

utilities/DraftFinalize.js
@@ -1,4 +1,4 @@
-// Start a draft on an existing object
+// Finalize an existing draft
 
 const Utility = require('./lib/Utility')
 

utilities/ImageSet.js
@@ -6,7 +6,7 @@ const Utility = require('./lib/Utility')
 const ExistObj = require('./lib/concerns/kits/ExistObj')
 const FabricFile = require('./lib/concerns/FabricFile')
 const Metadata = require('./lib/concerns/Metadata')
-const ArgFilePath = require('./lib/concerns/ArgFilePath')
+const ArgFilePath = require('./lib/concerns/args/ArgFilePath')
 
 const chkNoClearOrFilePath = (argv) => {
   if (!argv.clear && !argv.filePath) {

utilities/MetaDelete.js
@@ -2,20 +2,24 @@
 const objectPath = require('object-path')
 const R = require('@eluvio/ramda-fork')
 
+const {fabricItemDesc} = require('./lib/helpers')
 const {ModOpt, NewOpt} = require('./lib/options')
 
 const Utility = require('./lib/Utility')
 
-const ExistObj = require('./lib/concerns/kits/ExistObj')
+const ArgCommitMsg = require('./lib/concerns/args/ArgCommitMsg')
+const ExistLibOrObjOrDft = require('./lib/concerns/kits/ExistLibOrObjOrDft')
 const Metadata = require('./lib/concerns/Metadata')
 
 class MetaDelete extends Utility {
   static blueprint() {
     return {
-      concerns: [ExistObj, Metadata],
+      concerns: [ArgCommitMsg, ExistLibOrObjOrDft, Metadata],
       options: [
+        ModOpt('writeToken', {ofX: ' item to modify'}),
         ModOpt('objectId', {ofX: ' item to modify'}),
-        ModOpt('libraryId', {ofX: ' object to modify'}),
+        ModOpt('libraryId', {ofX: ' item to modify'}),
+        ModOpt('commitMsg', {conflicts: 'writeToken'}),
         NewOpt('path', {
           demand: true,
           descTemplate: 'Metadata path to delete (include leading \'/\')',
@@ -31,11 +35,16 @@ class MetaDelete extends Utility {
     // Check that paths are valid path strings
     Metadata.validatePathFormat({path})
 
-    const {libraryId, objectId} = await this.concerns.ExistObj.argsProc()
+    const {libraryId, objectId, writeToken} = await this.concerns.ExistLibOrObjOrDft.argsProc()
+    const commitMessage = this.args.commitMsg || (
+      this.args.writeToken
+        ? undefined
+        : `Delete metadata path: '${path}'`
+    )
     const currentMetadata = await this.concerns.ExistObj.metadata()
 
     // check to make sure path exists
-    if(!Metadata.pathExists({
+    if (!Metadata.pathExists({
       metadata: currentMetadata,
       path
     })) throw new Error(`Metadata path '${path}' not found.`)
@@ -46,19 +55,22 @@ class MetaDelete extends Utility {
 
     // Write back metadata
     const newHash = await this.concerns.Metadata.write({
+      commitMessage,
       libraryId,
       metadata: revisedMetadata,
-      objectId
+      objectId,
+      writeToken
     })
-    this.logger.data('version_hash', newHash)
+
+    if (!writeToken) this.logger.data('version_hash', newHash)
   }
 
   header() {
-    return `Delete metadata path '${this.args.path}' from object ${this.args.objectId}`
+    return `Delete metadata path '${this.args.path}' from ${fabricItemDesc(this.args)}`
   }
 }
 
-if(require.main === module) {
+if (require.main === module) {
   Utility.cmdLineInvoke(MetaDelete)
 } else {
   module.exports = MetaDelete

utilities/MetaSet.js
@@ -2,22 +2,28 @@
 const objectPath = require('object-path')
 const R = require('@eluvio/ramda-fork')
 
+const {fabricItemDesc} = require('./lib/helpers')
 const {ModOpt, NewOpt} = require('./lib/options')
 const Utility = require('./lib/Utility')
 
-const ExistObjOrDft = require('./lib/concerns/kits/ExistObjOrDft')
+const ExistLibOrObjOrDft = require('./lib/concerns/kits/ExistLibOrObjOrDft')
 const Metadata = require('./lib/concerns/Metadata')
 const ArgMetadata = require('./lib/concerns/ArgMetadata')
+const ArgCommitMsg = require('./lib/concerns/args/ArgCommitMsg')
 
 class MetaSet extends Utility {
   static blueprint() {
     return {
       concerns: [
-        ExistObjOrDft,
+        ExistLibOrObjOrDft,
         Metadata,
-        ArgMetadata
+        ArgMetadata,
+        ArgCommitMsg
       ],
       options: [
+        ModOpt('writeToken', {ofX: ' item to modify'}),
+        ModOpt('objectId', {ofX: ' item to modify'}),
+        ModOpt('libraryId', {ofX: ' item to modify'}),
         NewOpt('path', {
           demand: true,
           descTemplate: 'Path within metadata to set (start with \'/\').',
@@ -27,7 +33,8 @@ class MetaSet extends Utility {
           descTemplate: 'If target metadata path within object exists, overwrite and replace',
           type: 'boolean'
         }),
-        ModOpt('metadata', {demand:true})
+        ModOpt('metadata', {demand:true}),
+        ModOpt('commitMsg', {conflicts: 'writeToken'}),
       ]
     }
   }
@@ -36,6 +43,11 @@ class MetaSet extends Utility {
     const logger = this.logger
     const {path, force} = this.args
 
+    const commitMessage = this.args.commitMsg || (
+      this.args.writeToken
+        ? undefined
+        : `Set metadata path: '${path}'`
+    )
     // Check that path is a valid path string
     Metadata.validatePathFormat({path})
 
@@ -43,10 +55,10 @@ class MetaSet extends Utility {
 
     // operations that may need to wait on network access
     // ----------------------------------------------------
-    const {libraryId, objectId, writeToken} = await this.concerns.ExistObjOrDft.argsProc()
+    const {libraryId, objectId, writeToken} = await this.concerns.ExistLibOrObjOrDft.argsProc()
 
     logger.log('Retrieving existing metadata from object...')
-    const currentMetadata = await this.concerns.ExistObjOrDft.metadata()
+    const currentMetadata = await this.concerns.ExistLibOrObjOrDft.metadata()
 
     // check that targetPath can be set
     Metadata.validateTargetPath({
@@ -66,16 +78,18 @@ class MetaSet extends Utility {
 
     // Write back metadata
     const newHash = await this.concerns.Metadata.write({
+      commitMessage,
       libraryId,
       metadata: revisedMetadata,
       objectId,
       writeToken
     })
-    this.logger.data('version_hash', newHash)
+
+    if (!writeToken) this.logger.data('version_hash', newHash)
   }
 
   header() {
-    return `Set metadata ${this.args.path ? `at ${this.args.path} ` : ''}for object ${this.args.objectId}`
+    return `Set metadata ${this.args.path ? `at ${this.args.path} ` : ''}for ${fabricItemDesc(this.args)}`
   }
 }
 

utilities/MezCopyOffering.js
@@ -1,7 +1,7 @@
 const Utility = require('./lib/Utility')
 const {ModOpt, NewOpt} = require('./lib/options')
 
-const ArgOfferingKey = require('./lib/concerns/ArgOfferingKey')
+const ArgOfferingKey = require('./lib/concerns/args/ArgOfferingKey')
 const Edit = require('./lib/concerns/Edit')
 const ExistObj = require('./lib/concerns/kits/ExistObj')
 

commit cbe1cd583334346ef39569eeba01fb07e7763045

Fix write token bug in FilesAdd.js, introduced in 61bfb5b

utilities/FilesAdd.js
@@ -49,10 +49,10 @@ class FilesAdd extends Utility {
 
     const {libraryId, objectId, writeToken} = await this.concerns.ExistObjOrDft.argsProc()
 
-    const suppliedOrNewWriteToken = writeToken || await this.concerns.Edit.getWriteToken({
+    const suppliedOrNewWriteToken = writeToken || (await this.concerns.Edit.getWriteToken({
       libraryId,
       objectId
-    })
+    })).writeToken
 
     if(access) {
       await this.concerns.CloudFile.add({